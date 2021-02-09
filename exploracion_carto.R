a1f236b87d531988ffec42c1bfab8c96709b84a4
miusuario <miusuario@micorreo.com>
2021-02-08 22:58:23 -0600

    Arregla lectura de csv

--- exploracion_carto.R
+++ exploracion_carto.R
@@ -1,15 +1,16 @@
 library(readr)
+library(dplyr)
 
 
 carto_data <- read_csv(
   "table_5d787653.csv",
   col_types = cols(
-    cartodb_id = col_integer(),
-    id = col_character(),
-    follower_count = col_integer()
-))
-
-str(carto_data)
-
-carto_data$text
-
+    the_geom = col_skip(),
+    cartodb_id = col_skip(), id = col_skip(),
+    media = col_skip(), urls = col_skip(),
+    geo = col_skip(), time_zone = col_skip(),
+    location = col_skip(), source = col_skip(),
+    profile_image = col_skip(), language = col_skip(),
+    cartodb_georef_status = col_skip()
+  )
+)
